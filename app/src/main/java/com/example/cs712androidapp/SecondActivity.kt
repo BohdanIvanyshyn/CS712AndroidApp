702c8c7846e56d82824dc14da783adaed9ef7b83
boivanysh <ivanyshyn.bogdan@gmail.com>
2026-02-10 00:31:56 -0600

    assignment 2

---
+++ app/src/main/java/com/example/cs712androidapp/SecondActivity.kt
@@ -0,0 +1,29 @@
+package com.example.cs712androidapp
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+
+        // Edge-to-edge padding
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Main Activity button
+        val btnMainActivity = findViewById<Button>(R.id.btnMainActivity)
+        btnMainActivity.setOnClickListener {
+            finish()
+        }
+    }
+}
